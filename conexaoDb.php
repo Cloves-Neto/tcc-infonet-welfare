77d8ee313ca6c915e8abd1b42026ea094a4bdfed
Cloves Neto <cvr.neto20@gmail.com>
2023-03-25 08:36:51 -0300

    atualizando conexao bd

---
+++ conexaoDb.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'welfare_db';
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+
+
+// if($conexao -> connect_errno){
+//     echo "erro";
+// }else{
+//     echo "conexao ok";
+// }
+
+?>
