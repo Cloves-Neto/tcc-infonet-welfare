a6ac4f487df94e60905ac6923ba33d2c091ab5ed
Cloves Neto <cvr.neto20@gmail.com>
2023-05-03 16:11:14 -0300

    edit conect

--- logincontrole.php
+++ logincontrole.php
@@ -7,9 +7,15 @@ include_once("conexao.php");
 $email_funcionario = $_POST['email_funcionario'];
 $senha_funcionario = $_POST['senha_funcionario'];
 
+<<<<<<< Updated upstream
+=======
+// Conecta ao banco de dados
+// $conexao = new PDO("mysql:host=localhost; dbname=$Bco", "$Usuario", "$Senha"); 
+>>>>>>> Stashed changes
 
 try {
 
+<<<<<<< Updated upstream
     // Faz a validação no banco de dados
     $sql = "SELECT * FROM funcionario WHERE email_funcionario = '$email_funcionario' AND senha_funcionario = '$senha_funcionario'";
     $resultado = $conexao->query($sql);
@@ -29,3 +35,20 @@ try {
 } catch (PDOException $e) {
     echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
 }
+=======
+if ($resultado->num_rows == 1) {
+    // Usuário autenticado com sucesso
+    $usuario = $resultado->fetch_assoc();
+    session_start();
+    $_SESSION['email_funcionario'] = $usuario['email_funcionario'];
+    $_SESSION['senha_funcionario'] = $usuario['senha_funcionario'];
+    header('Location: ./home/red.php');
+} else {
+    // Login inválido
+    echo'<script>
+    alert ("Login invalido verifique suas credenciais")
+    </script>';
+    header('Location: index.php');
+    exit;
+}
+>>>>>>> Stashed changes
