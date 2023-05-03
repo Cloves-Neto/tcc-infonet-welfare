d5118f991934bd77577011d7c21061595924f361
JuliaVilha <juliasantanavilha@gmail.com>
2023-05-03 16:08:28 -0300

    senhor

--- logincontrole.php
+++ logincontrole.php
@@ -1,27 +1,31 @@
 
 <?php
 
-include_once ("conexao.php");
+include_once("conexao.php");
+
 // Recebe os dados do formulário
 $email_funcionario = $_POST['email_funcionario'];
 $senha_funcionario = $_POST['senha_funcionario'];
 
-// Conecta ao banco de dados
-$conexao = new PDO("mysql:host=localhost; dbname=$Bco", "$Usuario", "$Senha"); 
 
-// Faz a validação no banco de dados
-$sql = "SELECT * FROM funcionario WHERE email_funcionario = '$email_funcionario' AND senha_funcionario = '$senha_funcionario'";
-$resultado = $conexao->query($sql);
+try {
 
-if ($resultado->num_rows == 1) {
-    // Usuário autenticado com sucesso
-    $usuario = $resultado->fetch_assoc();
-    session_start();
-    $_SESSION['email_funcionario'] = $usuario['email_funcionario'];
-    $_SESSION['senha_funcionario'] = $usuario['senha_funcionario'];
-    header('Location: ./home/red.php');
-} else {
-    // Login inválido
-    header('Location: index.php');
-    exit;
+    // Faz a validação no banco de dados
+    $sql = "SELECT * FROM funcionario WHERE email_funcionario = '$email_funcionario' AND senha_funcionario = '$senha_funcionario'";
+    $resultado = $conexao->query($sql);
+
+    if ($resultado->rowCount() == 1) {
+        // Usuário autenticado com sucesso
+        $usuario = $resultado->fetch(PDO::FETCH_ASSOC);
+        session_start();
+        $_SESSION['email_funcionario'] = $usuario['email_funcionario'];
+        $_SESSION['senha_funcionario'] = $usuario['senha_funcionario'];
+        header('Location: ');
+    } else {
+        // Login inválido
+        header('Location: index.php');
+        exit;
+    }
+} catch (PDOException $e) {
+    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
 }
