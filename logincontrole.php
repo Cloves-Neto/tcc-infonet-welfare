351d8f7a40bcc90cfec48aa0d45b0ae2bcecb644
Cloves Neto <cvr.neto20@gmail.com>
2023-05-03 17:34:42 -0300

    edit conect

--- logincontrole.php
+++ logincontrole.php
@@ -6,13 +6,6 @@ include_once("conexao.php");
 // Recebe os dados do formulário
 $email_funcionario = $_POST['email_funcionario'];
 $senha_funcionario = $_POST['senha_funcionario'];
-
-<<<<<<< Updated upstream
-=======
-// Conecta ao banco de dados
-// $conexao = new PDO("mysql:host=localhost; dbname=$Bco", "$Usuario", "$Senha"); 
->>>>>>> Stashed changes
-
 try {
 
 <<<<<<< Updated upstream
@@ -35,20 +28,4 @@ try {
 } catch (PDOException $e) {
     echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
 }
-=======
-if ($resultado->num_rows == 1) {
-    // Usuário autenticado com sucesso
-    $usuario = $resultado->fetch_assoc();
-    session_start();
-    $_SESSION['email_funcionario'] = $usuario['email_funcionario'];
-    $_SESSION['senha_funcionario'] = $usuario['senha_funcionario'];
-    header('Location: ./home/red.php');
-} else {
-    // Login inválido
-    echo'<script>
-    alert ("Login invalido verifique suas credenciais")
-    </script>';
-    header('Location: index.php');
-    exit;
-}
->>>>>>> Stashed changes
+
