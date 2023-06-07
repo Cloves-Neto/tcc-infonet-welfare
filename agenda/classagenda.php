71e32ac6260e5d23c9c65fc8f20bfa9e5430d045
Cloves Neto <cvr.neto20@gmail.com>
2023-06-07 18:21:50 -0300

    att agd data

--- agenda/classagenda.php
+++ agenda/classagenda.php
@@ -4,28 +4,59 @@
 
 class Agenda{
 
-    
+// Busca médico
+
+    public function(){
+        
+    }
 
 
-
-
-
-
+// Data 
     public function dataCount(){
         // Inicio função datalist
         $inicio = new DateTime(); //chama a data atual é atualizada diariamente 
-        $fim = new DateTime('+1 year'); // chama a data atual +1 ano de intervalo e retorna um array
+        $fim = new DateTime('+60 days'); // chama a data atual +1 ano de intervalo e retorna um array
 
         $periodo = new DatePeriod($inicio, new DateInterval('P1D'), $fim);
-        $validos = [];
+        $data = [];
         foreach($periodo as $item){
-
-            if(substr($item->format("D"), 0, 1) != 'S'){
-                $validos[] = $item->format('d/m/Y');
-            }
+            $data[] = $item->format('d/m/Y');
         }
+        return $data; 
+    }
 
-        return $validos; // para printar os dados do array em uma parte do html chama a $validos[posição do item];
+
+
+
+
+
+    // =======================================================
+
+    public function SalvaMensagem(){
+        
+        $bd = new ConexaoBd();
+        $conn = $bd->getconexao();
+        $msg = $this->Mensagem;
+        $query = "INSERT INTO salvamsg (msg) VALUES (?)";
+        
+        $stm = $conn->prepare($query);
+        $stm->bindValue(1,$msg);
+
+        $stm->execute();
+        return $stm->rowCount();
+    }
+
+    public function ExcluiMensagem(){
+        $bd = new ConexaoBd();
+        $conn = $bd->getconexao();
+        $msg = $this->Id;
+        $query = "DELETE FROM salvamsg WHERE id_msg=?";
+        
+        $stm = $conn->prepare($query);
+        $stm->bindValue(1,$msg);
+
+        $stm->execute();
+        return $stm->rowCount();        
     }
 
 }
